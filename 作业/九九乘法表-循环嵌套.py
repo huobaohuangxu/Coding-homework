4f9b8074d57e63ecf92c650de51454350a0c3629
huobaohuangxu <1224724330@qq.com>
2020-11-15 19:22:41 +0800

    Update 九九乘法表-循环嵌套.py

--- 作业/九九乘法表-循环嵌套.py
+++ 作业/九九乘法表-循环嵌套.py
@@ -2,5 +2,5 @@
 #输出九九乘法表的左下三角
 for i in range(1,10):
     for j in range(1,i+1):
-        print('{0}*{1}={2} '.format(i,j,'%2d'%(i*j)),end="\t")
+        print('{0}*{1}={2} '.format(i,j,'%2d'%(i*j)),end=" ")
     print()
