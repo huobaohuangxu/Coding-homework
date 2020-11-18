500ba4985a675b491a3abc87103984d7705235d5
huobaohuangxu <1224724330@qq.com>
2020-11-18 08:38:31 +0800

    Update 斐波那契数列（右对齐）.py

--- 代码/斐波那契数列（右对齐）.py
+++ 代码/斐波那契数列（右对齐）.py
@@ -1,6 +1,6 @@
 a,b=0,1
 for i in range(1,21):
-    a,b=b,a+b
-    print('{0:6d}'.format(a),end=" ")
+    a,b = b,a+b#一一相对应
+    print('{0:6d}'.format(a),end=" ")#左补齐，留空位
     if i % 4==0:
-        print()
+        print()#打印空行
