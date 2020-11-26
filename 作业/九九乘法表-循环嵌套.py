f6e6cfe42384ea2666214ae55dd7ca14b1ee9314
huobaohuangxu <1224724330@qq.com>
2020-11-26 19:37:38 +0800

    Update 九九乘法表-循环嵌套.py

--- 作业/九九乘法表-循环嵌套.py
+++ 作业/九九乘法表-循环嵌套.py
@@ -1,5 +1,5 @@
-#九九乘法表-循环嵌套 http://oj.yangzhu.world/problem/haskell
-#输出九九乘法表的左下三角
+'''九九乘法表-循环嵌套 http://oj.yangzhu.world/problem/haskell
+输出九九乘法表的左下三角'''
 for i in range(1,10):
     for j in range(1,i+1):
         print('{0}*{1}={2} '.format(i,j,'%2d'%(i*j)),end=" ")
