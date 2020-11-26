ad9f666f7f8f33976aa87dd7bbe9d9b744badedc
huobaohuangxu <1224724330@qq.com>
2020-11-26 19:39:53 +0800

    Update 还要补考吗-ifelif.py

--- 作业/还要补考吗-ifelif.py
+++ 作业/还要补考吗-ifelif.py
@@ -1,5 +1,7 @@
-#还要补考吗-ifelif http://oj.yangzhu.world/problem/makeupexam2
-#使用if多分支结构完成程序。从键盘输入你的考试成绩（整数），若介于60到100分之间，输出“pass”，介于0到60分之间，输出“retake exam”，其他情况输出“input error”
+'''还要补考吗-ifelif http://oj.yangzhu.world/problem/makeupexam2
+使用if多分支结构完成程序。
+从键盘输入你的考试成绩（整数），若介于60到100分之间，输出“pass”，
+介于0到60分之间，输出“retake exam”，其他情况输出“input error”'''
 grade=int(input())
 if grade>=60 and grade<=100:
     print("pass")
