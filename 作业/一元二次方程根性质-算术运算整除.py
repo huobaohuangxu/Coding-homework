0fb0a595ac508bf401aa7d08eed4cb661013422e
huobaohuangxu <1224724330@qq.com>
2020-11-26 19:36:22 +0800

    Update 一元二次方程根性质-算术运算整除.py

--- 作业/一元二次方程根性质-算术运算整除.py
+++ 作业/一元二次方程根性质-算术运算整除.py
@@ -1,5 +1,6 @@
-#一元二次方程根性质-算术运算整除 http://oj.yangzhu.world/problem/rootproperty
-#从键盘输入一元二次方程ax²+bx+c=0的系数a、b、c（输入者保证输入整数且方程一定存在根），在不计算根x1和x2的情况下，输出（x1+x2）/2的结果和x1∗x2的结果，输出小数点后2位。
+'''一元二次方程根性质-算术运算整除 http://oj.yangzhu.world/problem/rootproperty
+从键盘输入一元二次方程ax²+bx+c=0的系数a、b、c（输入者保证输入整数且方程一定存在根），
+在不计算根x1和x2的情况下，输出（x1+x2）/2的结果和x1∗x2的结果，输出小数点后2位。'''
 a,b,c=map(int,input().split());
 d=b*b-4*a*c
 if d>=0:
