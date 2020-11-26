79c0de3e2bfe1972f90cfbe29e84c90e9029a678
huobaohuangxu <1224724330@qq.com>
2020-11-26 19:37:10 +0800

    Update 一元二次方程求根-if综合-checker.py

--- 作业/一元二次方程求根-if综合-checker.py
+++ 作业/一元二次方程求根-if综合-checker.py
@@ -1,8 +1,8 @@
-#一元二次方程求根-if综合-checker http://oj.yangzhu.world/problem/solvequation
-#从键盘输入一元二次方程的系数（整数），计算方程的根。
-#如果有2个不同的根，输出这2个根，精确到小数点后2位。(2个根的输出顺序不作限制)
-#如果有2个相同的根，输出这1个根，精确到小数点后2位。
-#如果没有根，输出“no root”
+'''一元二次方程求根-if综合-checker http://oj.yangzhu.world/problem/solvequation
+从键盘输入一元二次方程的系数（整数），计算方程的根。
+如果有2个不同的根，输出这2个根，精确到小数点后2位。(2个根的输出顺序不作限制)
+如果有2个相同的根，输出这1个根，精确到小数点后2位。
+如果没有根，输出“no root”'''
 from math import sqrt
 a,b,c=map(int,input().split());
 delta=b*b-4*a*c
